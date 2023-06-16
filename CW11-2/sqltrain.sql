7c4955e4bde6b3a236f5360dbc2330da533bf0e3
hosseink9 <hosseinkalantari.29@gmail.com>
2023-06-16 17:20:55 +0330

    Update file copleted part 3 about sorting with ASC and DESC

--- CW11-2/sqltrain.sql
+++ CW11-2/sqltrain.sql
@@ -1,5 +1,24 @@
 --Part 1
-SELECT * FROM film WHERE rental_duration >4;
+SELECT * FROM film WHERE rental_duration > 4;
 
 --part 2
 SELECT * FROM film WHERE rental_duration > 2 AND  rental_duration < 5;
+
+--part 3
+SELECT * FROM film WHERE rental_duration > 4 ORDER BY title ASC;
+SELECT * FROM film WHERE rental_duration > 4 ORDER BY title DESC;
+
+SELECT * FROM film WHERE rental_duration > 4 ORDER BY rental_duration ASC;
+SELECT * FROM film WHERE rental_duration > 4 ORDER BY rental_duration DESC;
+
+SELECT * FROM film WHERE rental_duration > 4 ORDER BY last_update ASC;
+SELECT * FROM film WHERE rental_duration > 4 ORDER BY last_update DESC;
+
+SELECT * FROM film WHERE rental_duration > 2 AND  rental_duration < 5 ORDER BY title ASC;
+SELECT * FROM film WHERE rental_duration > 2 AND  rental_duration < 5 ORDER BY title DESC;
+
+SELECT * FROM film WHERE rental_duration > 2 AND  rental_duration < 5 ORDER BY rental_duration ASC;
+SELECT * FROM film WHERE rental_duration > 2 AND  rental_duration < 5 ORDER BY rental_duration DESC;
+
+SELECT * FROM film WHERE rental_duration > 2 AND  rental_duration < 5 ORDER BY last_update ASC;
+SELECT * FROM film WHERE rental_duration > 2 AND  rental_duration < 5 ORDER BY last_update DESC;
