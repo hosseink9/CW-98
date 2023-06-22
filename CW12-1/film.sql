b1c91492ad2d9e18d69dc65d28f877f9fc3c3f52
hosseink9 <hosseinkalantari.29@gmail.com>
2023-06-22 14:33:29 +0330

    Updated film.sql Fixed file

--- CW12-1/film.sql
+++ CW12-1/film.sql
@@ -1,4 +1,4 @@
 --part 1
-SELECT film.title,film.release_year,category.name FROM film
+SELECT film.title,film.release_year,(category.name) AS category FROM film
 INNER JOIN film_category ON film_category.film_id=film.film_id
 INNER JOIN category ON category.category_id = film_category.category_id;
