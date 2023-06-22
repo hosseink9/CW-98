edb072d7ce7fcb0fbd0f8a1f6e4bad6af30e23ff
hosseink9 <hosseinkalantari.29@gmail.com>
2023-06-22 15:46:27 +0330

    Updated film.sql copmleted part 7

--- CW12-1/film.sql
+++ CW12-1/film.sql
@@ -34,3 +34,7 @@ INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
 INNER JOIN film ON film.film_id = inventory.inventory_id;
 
 
+--part 7
+
+select * from film
+where film.length > (select avg(length) from film);
