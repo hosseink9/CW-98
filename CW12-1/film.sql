5387f6cc07cdc0263858eccebfd84e4ec863ab42
hosseink9 <hosseinkalantari.29@gmail.com>
2023-06-22 14:28:16 +0330

    Upload film.sql file for CW1 Week 12 completed part 1

---
+++ CW12-1/film.sql
@@ -0,0 +1,4 @@
+--part 1
+SELECT film.title,film.release_year,category.name FROM film
+INNER JOIN film_category ON film_category.film_id=film.film_id
+INNER JOIN category ON category.category_id = film_category.category_id;
