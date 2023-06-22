756bca4ab6c69d07899d24c5590855699a662fe2
hosseink9 <hosseinkalantari.29@gmail.com>
2023-06-22 15:23:49 +0330

    Updated film.sql Completed all part and add part 6

--- CW12-1/film.sql
+++ CW12-1/film.sql
@@ -27,4 +27,10 @@ INNER JOIN film ON film.language_id = language.language_id
 INNER JOIN film_category ON film_category.film_id=film.film_id
 INNER JOIN category ON category.category_id = film_category.category_id;
 
+--part 6
+SELECT customer.customer_id,customer.first_name,customer.last_name,(rental.return_date - rental.rental_date) AS rental_days, film.title FROM customer
+INNER JOIN rental ON rental.customer_id = customer.customer_id
+INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+INNER JOIN film ON film.film_id = inventory.inventory_id;
+
 
