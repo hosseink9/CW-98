f6f8d73bd3f7cce9391b0e173b89cf8f5d55538e
hosseink9 <hosseinkalantari.29@gmail.com>
2023-07-13 17:17:13 +0330

    add some posts and complete project

--- CW15/BlogPlatform/blogapp/models.py
+++ CW15/BlogPlatform/blogapp/models.py
@@ -4,14 +4,23 @@ class Post(models.Model):
     title=models.CharField(max_length=255,null=False)
     content=models.CharField(max_length=255,null=False)
     author=models.CharField(max_length=255,null=False)
-    publication_date=models.DateField(auto_now_add=False)
+    publication_date=models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
 
 class Category(models.Model):
     name=models.CharField(max_length=255,)
     description=models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
 
 class Comment(models.Model):
     post=models.CharField(max_length=255)
     author=models.CharField(max_length=255,null=False)
     content=models.CharField(max_length=255)
     date=models.DateField(auto_now_add=False)
+
+    def __str__(self) -> str:
+        return self.post
--- CW15/BlogPlatform/blogapp/views.py
+++ CW15/BlogPlatform/blogapp/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 def home(request):
     return render(request,'home.html')
+
+def all_post(request):
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, "post.html", context)
