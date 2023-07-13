f2eae30e444109b2a3e8be318baea1afe1f2e5ca
hosseink9 <hosseinkalantari.29@gmail.com>
2023-07-13 15:47:01 +0330

    add and complete some files

--- CW15/BlogPlatform/blogapp/models.py
+++ CW15/BlogPlatform/blogapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+#Category Model: Fields: name, description, and any other relevant information.
+# o Comment Model: Fields: post, author, content, date, and any other relevant information.
+
+class Post(models.Model):
+    title=models.CharField(max_length=255,null=True)
+    content=models.CharField(max_length=255,null=True)
+    author=models.CharField(max_length=255,null=True)
+    publication_date=models.DateField(auto_now_add=True)
+
+class Category(models.Model):
+    name=models.CharField(max_length=255,)
--- CW15/BlogPlatform/blogapp/views.py
+++ CW15/BlogPlatform/blogapp/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'home.html',{'name':'Hossein'})
+    return render(request,'home.html')
