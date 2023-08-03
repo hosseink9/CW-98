1e1fe3c74a3a8af907b5c0f74b73e409140bd911
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-03 16:14:52 +0330

    add passwordinput and fields

--- CW16/task_management/userapp/forms.py
+++ CW16/task_management/userapp/forms.py
@@ -1,6 +1,8 @@
-from django.forms import ModelForm
+from django.forms import ModelForm,PasswordInput,CharField
 from models import *
 
 class UserForm(ModelForm):
+    passwordinput=CharField(widget=PasswordInput)
     class Meta:
         model=User
+        fields=('username','password','email')
