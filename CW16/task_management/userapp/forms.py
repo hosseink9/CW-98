0e32eb520914b85edd58aef6bb64cf482e0b65ad
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-03 16:15:28 +0330

    add password check and confirm_password

--- CW16/task_management/userapp/forms.py
+++ CW16/task_management/userapp/forms.py
@@ -1,8 +1,15 @@
-from django.forms import ModelForm,PasswordInput,CharField
+from django.forms import ModelForm,PasswordInput,CharField, ValidationError
 from models import *
 
 class UserForm(ModelForm):
-    passwordinput=CharField(widget=PasswordInput)
+    confirm_password=CharField(widget=PasswordInput)
     class Meta:
         model=User
         fields=('username','password','email')
+
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+        if password != confirm_password:
+            raise ValidationError("password and confirm_password does not match")
