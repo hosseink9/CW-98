995b6ed923430dcbf9216c133cc5b59dd1146639
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-04 14:45:35 +0330

    add view for user

--- CW16/task_management/userapp/views.py
+++ CW16/task_management/userapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import User
+from .forms import UserForm,UserLoginForm
 
-# Create your views here.
+def userlogin(request):
+    if request.method["POST"]:
+        form=UserLoginForm(request.POST)
+        if form.is_valid():
+            clean_d=form.cleaned_data
+            username=clean_d['username']
+            password=clean_d['password']
+    else:
+        form=UserLoginForm()
+        return render(request, 'login.html',{'form':form})
