cbba58c3df0550be80edff8d9e4a433b9dc37c18
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-04 15:01:32 +0330

    chor: complete userlogin view

--- CW16/task_management/userapp/views.py
+++ CW16/task_management/userapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import User
 from .forms import UserForm,UserLoginForm
+from .auth import AuthBackend
+from django.contrib.auth import login,authenticate
 
 def userlogin(request):
     if request.method["POST"]:
@@ -9,6 +11,12 @@ def userlogin(request):
             clean_d=form.cleaned_data
             username=clean_d['username']
             password=clean_d['password']
+            user=AuthBackend.authenticate(username=username,password=password)
+            if user:
+                login(request,user)
+                return redirect('home')
+            else:
+                form.add_error(None,"your input is invalid")
     else:
-        form=UserLoginForm()
-        return render(request, 'login.html',{'form':form})
+        form = UserLoginForm()
+    return render(request,'epic_quest/login.html',{'form':form})
