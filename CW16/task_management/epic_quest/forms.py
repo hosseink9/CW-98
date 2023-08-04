f67d20b701d6251ba32689173a480f6695a7a515
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-04 17:15:43 +0330

    chor: add forms for epic quest

--- CW16/task_management/epic_quest/forms.py
+++ CW16/task_management/epic_quest/forms.py
@@ -1,5 +1,14 @@
 from django import forms
 from django.forms import ModelForm
 from .models import *
+from task.models import *
 
-class CategoryForm(models.Model):
+class CategoryForm(ModelForm):
+    class Meta:
+        model=Category
+        fields=('name','description')
+
+class TagForm(ModelForm):
+    class Meta:
+        model=Tag
+        fields=('name',)
