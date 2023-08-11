900d2badc97bceac1a383815c461b97b39e4a35f
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-11 14:46:13 +0330

    completed part 2 cw

--- CW20/cw.js
+++ CW20/cw.js
@@ -9,3 +9,15 @@
 
 
 // 2)
+let age = prompt("Enter your age: ");
+let toint = parseInt(age);
+
+if (age >= 0 && age <= 10) {
+    console.log("child");
+} else if (age >= 21 && age <= 18) {
+    console.log("Teen");
+} else if (age >= 19 && age <= 30) {
+    console.log("Adult");
+} else {
+    console.log("Old");
+}
