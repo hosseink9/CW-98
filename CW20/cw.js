f79a31b4dbcbdeb03f27873dad89fd12ab57fdbb
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-11 14:39:42 +0330

    completed part 1 cw

---
+++ CW20/cw.js
@@ -0,0 +1,11 @@
+// 1)
+// let userinput = prompt('Enter something: ')
+// let userinputtype = parseInt(userinput)
+// if (isNaN(userinputtype)) {
+//     console.log('string')
+// } else {
+//     console.log(`type of what you entered: ${typeof userinputtype}`)
+// }
+
+
+// 2)
