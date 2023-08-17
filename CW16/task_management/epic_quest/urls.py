4031cccb0cd1010b166ed96c542a43778904c71f
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-17 15:00:48 +0330

    change all_view to AllView

--- CW16/task_management/epic_quest/urls.py
+++ CW16/task_management/epic_quest/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 from django.contrib import admin
 
-admin.site.site_header='ramin'
-urlpatterns=[
+admin.site.site_header = 'ramin'
+urlpatterns = [
     path('', views.home, name='home'),
-    path("view_all", views.view_all, name='view_all'),
+    path("view_all", views.AllView.as_view(), name='view_all'),
     path("category/", views.all_category, name="all_category"),
     path("category/<int:pk>", views.category_details, name="category_details"),
     path("search/", views.search, name="search"),
