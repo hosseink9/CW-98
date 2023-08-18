45d89ea1fd3ae65bfc01bb23ae488165f9bae9af
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-18 12:12:32 +0330

    add read only fields for created and updated at

--- CW16/task_management/task/admin.py
+++ CW16/task_management/task/admin.py
@@ -2,18 +2,22 @@ from django.contrib import admin
 from . import models
 from django.contrib.sessions.models import Session
 
+
 @admin.register(models.Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display=['title','due_date','status_fields','category']
-    list_editable=['status_fields']
-    ordering = ['title','status_fields']
+    list_display = ['title', 'due_date', 'status_fields', 'category']
+    readonly_fields = ["created_at", "updated_at"]
+    list_editable = ['status_fields']
+    ordering = ['title', 'status_fields']
     # list_select_related=['category']
     list_per_page = 4
+
 
 class SessionAdmin(admin.ModelAdmin):
     def sessiondata(self, obj):
         return obj.get_decoded()
-    
-    list_display=['session_key','sessiondata']
 
-admin.site.register(Session,SessionAdmin)
+    list_display = ['session_key', 'sessiondata']
+
+
+admin.site.register(Session, SessionAdmin)
