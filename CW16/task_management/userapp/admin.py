3b66bb0c4011c15f12245225b4f0a9fac40f5a8e
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-18 10:57:01 +0330

    add is_greate_user in useradmin

--- CW16/task_management/userapp/admin.py
+++ CW16/task_management/userapp/admin.py
@@ -5,9 +5,16 @@ from task.models import Task
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ["username", 'is_active', 'number_of_task']
+    list_display = ["username", 'is_active',
+                    'number_of_task', 'is_greate_user']
 
     @admin.display(ordering="username")
     def number_of_task(self, user):
         query = Task.objects.filter(user=user).count()
         return query
+
+    def is_greate_user(self, user):
+        query = Task.objects.filter(user=user).count()
+        if query > 10:
+            return True
+        return False
