b98fc5eab9b32da60048c047e6c801fdb3e05022
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-18 11:23:35 +0330

    add filter and list filter for adminview

--- CW16/task_management/userapp/admin.py
+++ CW16/task_management/userapp/admin.py
@@ -3,10 +3,23 @@ from .models import User
 from task.models import Task
 
 
+class AdminFilter(admin.SimpleListFilter):
+    title = "Is_greate_user"
+    parameter_name = "greate"
+
+    def lookups(self, request, model_admin):
+        return [('greate', 'Is_greate_user')]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'greate':
+            return queryset.filter(is_greate_user=True)
+
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["username", 'is_active',
                     'number_of_task', 'is_greate_user']
+    list_filter = [AdminFilter]
 
     @admin.display(ordering="username")
     def number_of_task(self, user):
@@ -18,3 +31,4 @@ class UserAdmin(admin.ModelAdmin):
         if query > 10:
             return True
         return False
+    is_greate_user.boolean = True
