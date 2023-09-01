06890d0b17e7239d33e35173492b69744ba7a318
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-01 10:35:01 +0330

    created artist model

--- MusicPlayer/src/artists/models.py
+++ MusicPlayer/src/artists/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.TextField(blank=True,null=True)
+    image = models.ImageField(upload_to='images/artist', null=True)
+
+    def __str__(self) -> str:
+        return self.name
