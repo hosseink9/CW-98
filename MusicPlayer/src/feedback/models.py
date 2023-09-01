231af69ac762b3850cd0849e8f8e93101d536c1b
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-01 09:54:25 +0330

    Created Like and Comment models

--- MusicPlayer/src/feedback/models.py
+++ MusicPlayer/src/feedback/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from users.models import User
+from geners.models import Song
 
-# Create your models here.
+class Like(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    song = models.ForeignKey(Song,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.song
+
+class Comment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    song = models.ForeignKey(Song,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return
+
+    def verify(self, admin:User):
+        self.verify = admin
+        self.save()
