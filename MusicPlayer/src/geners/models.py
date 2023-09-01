2ec32e729a569ef9dc0659eb3e7b476cd11e2ac5
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-01 08:55:04 +0330

    fix the name of basic file

--- MusicPlayer/src/geners/models.py
+++ MusicPlayer/src/geners/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from artists.models import Artist
+
 
 class Gener(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+class Song(models.Model):
+    title = models.CharField(max_length=60)
+    upload_date = models.DateField(auto_now_add=True)
+    cover_photo = models.ImageField(upload_to='images/cover_photo/', null=True)
+    audio_file = models.FileField()
+    artist = models.ManyToManyField(Artist)
+    geners = models.ForeignKey(Gener,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
