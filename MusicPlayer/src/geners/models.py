6e55d562ae038aa139269f2bd1670993ee8d3e0f
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-01 09:08:02 +0330

    completed song class

--- MusicPlayer/src/geners/models.py
+++ MusicPlayer/src/geners/models.py
@@ -11,8 +11,8 @@ class Gener(models.Model):
 class Song(models.Model):
     title = models.CharField(max_length=60)
     upload_date = models.DateField(auto_now_add=True)
-    cover_photo = models.ImageField(upload_to='images/cover_photo/', null=True)
-    audio_file = models.FileField()
+    cover_photo = models.ImageField(upload_to='images/cover_photo', null=True)
+    audio_file = models.FileField(upload_to='music')
     artist = models.ManyToManyField(Artist)
     geners = models.ForeignKey(Gener,on_delete=models.CASCADE)
 
