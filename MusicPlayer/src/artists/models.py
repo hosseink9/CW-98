e6012eca8a9ff400e711b181ef7691f9ab37e9f3
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-02 10:17:41 +0330

    add base model for artist model

--- MusicPlayer/src/artists/models.py
+++ MusicPlayer/src/artists/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from main.models import BaseModel
 
-class Artist(models.Model):
+class Artist(BaseModel):
     name = models.CharField(max_length=50)
     bio = models.TextField(blank=True,null=True)
     image = models.ImageField(upload_to='images/artist', null=True)
