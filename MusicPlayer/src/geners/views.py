287728b8835c3bca679b666f7c3f49cfa94c4d72
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-02 11:45:45 +0330

    change app name

--- MusicPlayer/src/geners/views.py
+++ MusicPlayer/src/geners/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import FormView,ListView
+from geners.models import *
 
-# Create your views here.
+
+class ListAllSongs(ListView):
+    template_name = ""
+    model = Song
+
