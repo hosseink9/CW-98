21ee6673516b71bde50b99cd4e5b5e77e7570c36
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-02 20:50:33 +0330

    add Signin view with create view generic

--- MusicPlayer/src/users/views.py
+++ MusicPlayer/src/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.generic import FormView,CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import User
+from .forms import *
+
+class SignInView(CreateView):
+
+    template_name = 'users/signin.html'
+    form_class = UserSignin
+    success_url = reverse_lazy("geners:song_list")
+
+    def dispatch(self, request, *args, **kwargs):
+        if isinstance(request.user, User):
+            return redirect("geners:song_list")
+        return super().dispatch(request, *args, **kwargs)
