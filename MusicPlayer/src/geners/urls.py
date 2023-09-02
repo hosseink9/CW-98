1d635df033c313c681c46abd552e588b03e6b99e
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-02 20:48:58 +0330

    add 2 urls for in geners url

--- MusicPlayer/src/geners/urls.py
+++ MusicPlayer/src/geners/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name = 'geners'
 
 urlpatterns = [
-    path('song_list', ListAllSongs.as_view(), name='song_list')
+    path('song_list', ListAllSongs.as_view(), name='song_list'),
+    path('song_detail/<int:pk>', DesplaySongDetail.as_view(), name='song_detail')
 ]
