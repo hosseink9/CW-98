f8fb72630cb3e0014ebd907163293f58fac4ba09
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-02 10:16:04 +0330

    add base model for all geners models

--- MusicPlayer/src/geners/models.py
+++ MusicPlayer/src/geners/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from artists.models import Artist
 from users.models import User
+from main.models import BaseModel
 
 
-class Gener(models.Model):
+class Gener(BaseModel):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
-class Song(models.Model):
+class Song(BaseModel):
     title = models.CharField(max_length=60)
     upload_date = models.DateField(auto_now_add=True)
     cover_photo = models.ImageField(upload_to='images/cover_photo', null=True)
@@ -21,7 +22,7 @@ class Song(models.Model):
         return self.title
 
 
-class Playlist(models.Model):
+class Playlist(BaseModel):
     title = models.CharField(max_length=40)
     description = models.TextField(blank=False,null=True)
     owner = models.ForeignKey(User,on_delete=models.CASCADE)
