0842343093cc8f233aae27e6dce9ebc08d77438b
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-02 10:18:17 +0330

    inherit base model for all models in feedback

--- MusicPlayer/src/feedback/models.py
+++ MusicPlayer/src/feedback/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from users.models import User
 from geners.models import Song
+from main.models import BaseModel
 
-class Like(models.Model):
+class Like(BaseModel):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     song = models.ForeignKey(Song,on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.song
 
-class Comment(models.Model):
+class Comment(BaseModel):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     song = models.ForeignKey(Song,on_delete=models.CASCADE)
     text = models.TextField(max_length=300)
