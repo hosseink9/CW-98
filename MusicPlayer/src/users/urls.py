cc321eb0000c28b0c30edc865235087ef271b5af
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-04 00:20:41 +0330

    add some urls

--- MusicPlayer/src/users/urls.py
+++ MusicPlayer/src/users/urls.py
@@ -4,5 +4,7 @@ from .views import *
 app_name = 'users'
 
 urlpatterns = [
-    path('/signin', SignInView.as_view(), name='signin')
+    path('/signin', SignInView.as_view(), name='signin'),
+    path('/login', LoginView.as_view(), name='login'),
+    path('/logout', logout_user,name='logout')
 ]
