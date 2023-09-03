b8ffe8093f31bc0f94b8cc760c24020f22afd303
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-04 00:19:41 +0330

    add some func

--- MusicPlayer/src/users/forms.py
+++ MusicPlayer/src/users/forms.py
@@ -1,9 +1,9 @@
 from django.forms import ModelForm,PasswordInput,CharField, ValidationError
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password,check_password
 from django import forms
 from .models import *
 
-class UserSignin(ModelForm):
+class UserSigninForm(ModelForm):
     confirm_password=CharField(widget=PasswordInput)
     password=CharField(max_length=30,widget=PasswordInput)
     class Meta:
@@ -11,15 +11,36 @@ class UserSignin(ModelForm):
         fields=('username','first_name','last_name','phone','account','photo','password')
 
     def clean(self):
-        cleaned_data = super(UserSignin, self).clean()
+        cleaned_data = super(UserSigninForm, self).clean()
         password = cleaned_data.get("password")
         confirm_password = cleaned_data.get("confirm_password")
         if password != confirm_password:
             raise ValidationError("password and confirm_password does not match")
-        cleaned_data['password']=make_password('password')
+        # cleaned_data['password']=make_password('password')
+        # cleaned_data['password']=password
         return cleaned_data
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
 class UserLoginForm(forms.Form):
-   class Meta:
-       model = User
-       fields = ('phone','password')
+    phone=forms.CharField(
+        validators = [phone_validator],
+        widget = forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': "Phone"
+        }))
+    password=CharField(max_length=30,widget=PasswordInput)
+
+    # def clean(self):
+    #     cleaned_data = super(UserSigninForm, self).clean()
+    #     password = cleaned_data.get("password")
+    #     confirm_password = cleaned_data.get("confirm_password")
+    #     if password != confirm_password:
+    #         raise ValidationError("password and confirm_password does not match")
+    #     cleaned_data['password']=make_password('password')
+    #     return cleaned_data
