347f1676733033f99bd7d85382de06d75482639b
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-05 19:14:05 +0330

    fix

--- MusicPlayer/src/conifg/urls.py
+++ MusicPlayer/src/conifg/urls.py
@@ -23,9 +23,11 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('geners.urls')),
-    path('users', include('users.urls'))
-
+    path('users', include('users.urls')),
     ]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
