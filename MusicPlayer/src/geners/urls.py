2443fb6289a08983275ac8fb505fdc33136a7efe
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-05 19:16:03 +0330

    fixed geners url

--- MusicPlayer/src/geners/urls.py
+++ MusicPlayer/src/geners/urls.py
@@ -4,7 +4,7 @@ from .views import *
 app_name = 'geners'
 
 urlpatterns = [
-    path('song_list', ListAllSongs.as_view(), name='song_list'),
+    path('', ListAllSongs.as_view(), name='song_list'),
     path('song_detail/<int:pk>', DesplaySongDetail.as_view(), name='song_detail'),
     path('createplaylist', CreatePlaylist.as_view(), name='createplaylist')
 ]
