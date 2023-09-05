0ea3fcdcc448c97072abed42731c681a323cdf3c
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-05 19:13:25 +0330

    add blank to image field

--- MusicPlayer/src/artists/models.py
+++ MusicPlayer/src/artists/models.py
@@ -4,7 +4,7 @@ from main.models import BaseModel
 class Artist(BaseModel):
     name = models.CharField(max_length=50)
     bio = models.TextField(blank=True,null=True)
-    image = models.ImageField(upload_to='images/artist', null=True)
+    image = models.ImageField(upload_to='images/artist', null=True, blank=True)
 
     def __str__(self) -> str:
         return self.name
